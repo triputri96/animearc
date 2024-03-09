76f74d5f21a73b3d7eb5cb64010862c6ad083cea
tini79 <artinichan79@gmail.com>
2024-03-09 19:50:59 +0800

    added homepage

---
+++ assets/js/script.js
@@ -0,0 +1,5 @@
+function dropMenu() {
+  const menuDropdown = document.getElementById('menuDropdown')
+  const menuList = document.getElementById('menuList')
+  menuList.classList.toggle('d-none')
+}
